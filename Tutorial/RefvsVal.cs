d52c1deb358370bf6080762d56eaaf6da7c620ed
Chiras Valentn <chiras.valentin.99@gmail.com>
2021-04-08 14:55:12 +0300

    Exemplu in care arat diferenta intre tipurile diferentiale si tipurile valorice

---
+++ Tutorial/RefvsVal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class RefvsVal
+{
+	class Class1
+	{
+		public indexert Val = 0;
+	}
+	struct Struct1
+    {
+		public int Val;
+    }
+
+}
---
+++ Tutorial/Tutorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tutorial
+{
+    class Class1
+    {
+        public int Val = 0;
+    }
+    struct Struct1
+    {
+        public int Val;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
